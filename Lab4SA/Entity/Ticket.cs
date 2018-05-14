efe70b8b30437ef4078ccdcabb0ca476a8658361
simachenko <simachenko@users.noreply.github.com>
2018-05-14 19:45:22 +0300

    ClassDiagramm++
    ++Diagramm

---
+++ Lab4SA/Entity/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Ticket
+    {
+        public int TicketId { set; get; }
+        public int ticketCode { set; get; }
+
+        public int? nameSpectacleId { set; get; }
+        public virtual Spectacle name { set; get; }
+        
+
+    }
+}
