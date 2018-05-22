3c2b63ee055ebf3ec91414a2e50bb8d1b0bf1a5f
vladislav.kyrylenko <b.c.s.kyrylenko@gmail.com>
2018-05-22 17:21:04 +0300

    BLLv1.0
    BLLv1.0

--- DataAccessLayer/Entities/TimetableEntity/Day.cs
+++ DataAccessLayer/Entities/TimetableEntity/Day.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
+    [Table("Lessons")]
     public class Day : Entity
     {
+        [Key]
         public int DayID { set; get; }
+        public string name { set; get; }
+        public int groupeID { set; get; }
+        public Group groupe { set; get; }
         public ICollection<Lesson> lesson { set; get; }
-        public int WeekId { set; get; }
-        public Week week { set; get; }
+        
     }
 }
